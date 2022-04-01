6b5f124f94c8f6cabe5dba83151ba88b0b961dbc
BBRON-ASUS <aaronzhang587@gmail.com>
2022-04-01 15:22:49 -0700

    Start

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+//Array of contacts
+let contactList = [
+    {
+      name: "Barry Allen",
+      phone: "+1 555 555-5555",
+      address: "123 front st, Unit #1, Dakota City",
+      email: "theflash@gmail.com",
+    },
+    {
+      name: "Beverly Crusher",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "chiefmedicalofficer@slack.example.com",
+    }, 
+    {
+      name: "Diana Prince",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "wonderwoman@doc.example.com",
+    }
+]

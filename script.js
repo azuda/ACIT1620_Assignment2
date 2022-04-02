304948c6d71d319248687385dd0600556f0462bf
liliu <sfey1@my.bcit.ca>
2022-04-01 18:15:18 -0700

    Add renderCreate() functionality to script.js

--- script.js
+++ script.js
@@ -25,6 +25,41 @@ function cleanUpIndex() {
   return elementArr.forEach((i) => i.remove());
 }
 
-function renderCreate() {
-  pass
+function renderCreate(contact) {
+  //create each element on page3.html and populate
+  let divmain = document.createElement('div');
+  divmain.setAttribute('class', 'main');
+  let header = document.getElementsByTagName('header')[0];
+  header.insertAdjacentElement('afterend', divmain);
+
+  let divcinfo = document.createElement('div');
+  divcinfo.setAttribute('class', 'contactinfo');
+  divmain.appendChild(divcinfo);
+
+  let divcname = document.createElement('div');
+  divcname.setAttribute('class', 'contactname');
+  divcname.innerHTML = contact.name + '<img src="./img/profile.jpg" class="profilepic" alt="Profile picture">';
+  divcinfo.appendChild(divcname);
+
+  let divcemail = document.createElement('div');
+  divcemail.setAttribute('class', 'contactemail');
+  divcemail.innerHTML = 'email: ' + contact.email;
+  divcinfo.appendChild(divcemail);
+
+  let divcphone = document.createElement('div');
+  divcphone.setAttribute('class', 'contactphone');
+  divcphone.innerHTML = 'cell: ' + contact.phone
+  divcinfo.appendChild(divcphone);
+
+  let divcadd = document.createElement('div');
+  divcadd.setAttribute('class', 'contactaddress');
+  divcadd.innerHTML = 'address: ' + contact.address;
+  divcinfo.appendChild(divcadd);
+
+  let divcbutt = document.createElement('div');
+  divcbutt.setAttribute('class', 'buttons');
+  divcbutt.innerHTML = '<button class="button edit" value="Edit">Edit</button><button class="button close" value="Close">Close</button>';
+  divcinfo.appendChild(divcbutt);
+
+  //renderCreate(contactList[0])
 }
